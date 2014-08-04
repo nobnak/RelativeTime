91aed409b1e22f791ae14f6e9be902926211b09b
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-08-04 19:42:30 +0900

    テクスチャのペイントで、時間経過を加速

---
+++ Assets/Attenuate.shader
@@ -0,0 +1,39 @@
+﻿Shader "Custom/Attenuate" {
+	Properties {
+		_Atten ("Attenuation", Float) = 0.01
+	}
+	SubShader {
+		ZTest Always ZWrite Off Cull Off Fog { Mode Off }
+		Blend Zero SrcAlpha
+		
+		Pass {
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			float _Atten;
+			
+			struct appdata {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			struct vs2ps {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+			
+			vs2ps vert(appdata IN) {
+				vs2ps OUT;
+				OUT.vertex = mul(UNITY_MATRIX_MVP, IN.vertex);
+				OUT.uv = IN.uv;
+				return OUT;
+			}
+			float4 frag(vs2ps IN) : COLOR {
+				return float4(0.0, 0.0, 0.0, saturate(1.0 - _Atten));
+			}
+			ENDCG
+		}
+	} 
+	FallBack off
+}
--- Assets/RelativeTime.cs
+++ Assets/RelativeTime.cs
@@ -7,6 +7,7 @@ public class RelativeTime : MonoBehaviour {
 	public const string CS_PROP_CLOCKS_PREV = "clocksPrev";
 	public const string CS_PROP_CLOCKS_CURR = "clocksCurr";
 	public const string CS_PROP_DELTA_TIME = "dt";
+	public const string CS_PROP_HEAT_TEX = "HeatTex";
 	public const int CS_NUM_THREADS = 256;
 
 	public const string LIFE_PROPE_CLOCKS_CURR = "clocksCurr";
@@ -17,11 +18,23 @@ public class RelativeTime : MonoBehaviour {
 	public ComputeShader relativeTimeCompute;
 	public GameObject flowerfab;
 
+	public Material brushMat;
+	public Texture2D brushTex;
+	public Material attenMat;
+
 	private int numGroups;
 	private Clock[] _clocks;
 	private ComputeBuffer _clocksBuffer0, _clocksBuffer1;
 	private GameObject[] _flowers;
+	private RenderTexture _paintWallTex;
 
+	void OnDestroy() {
+		if (_clocksBuffer0 != null)
+			_clocksBuffer0.Release();
+		if (_clocksBuffer1 != null)
+			_clocksBuffer1.Release();
+	}
+	
 	void Start() {
 		numGroups = Mathf.CeilToInt((float)numClocks / CS_NUM_THREADS);
 		numClocks = CS_NUM_THREADS * numGroups;
@@ -36,50 +49,35 @@ public class RelativeTime : MonoBehaviour {
 
 		_flowers = new GameObject[numClocks];
 
+		_paintWallTex = new RenderTexture(256, 256, 0, RenderTextureFormat.RFloat, RenderTextureReadWrite.Linear);
+		RenderTexture.active = _paintWallTex;
+		GL.Clear(true, true, Color.black);
+		RenderTexture.active = null;
+		renderer.sharedMaterial.mainTexture = _paintWallTex;
+
 		StartCoroutine(Cleaner());
 		StartCoroutine(Logger());
 	}
 
-	IEnumerator Cleaner() {
-		while (enabled) {
-			yield return new WaitForSeconds(1f);
-			_clocksBuffer0.GetData(_clocks);
-			for (var i = 0; i < _clocks.Length; i++) {
-				var c = _clocks[i];
-				if (c.t >= tEnd) {
-					c.t = -1f;
-					_clocks[i] = c;
-					Destroy(_flowers[i]);
-				}
-			}
-			_clocksBuffer0.SetData(_clocks);
-		}
-	}
-	IEnumerator Logger() {
-		var log = new StringBuilder();
-		while (enabled) {
-			yield return new WaitForSeconds(1f);
-			log.Length = 0;
-			_clocksBuffer0.GetData(_clocks);
-			for (var i = 0; i < 100; i++) {
-				var c = _clocks[i];
-				if (c.t < 0 || c.t >= tEnd)
-					continue;
-				log.AppendFormat("[{0:f}] ", c.t);
-			}
-			Debug.Log(log.ToString());
-		}
-	}
-
-	void OnDestroy() {
-		if (_clocksBuffer0 != null)
-			_clocksBuffer0.Release();
-		if (_clocksBuffer1 != null)
-			_clocksBuffer1.Release();
-	}
-
 	void Update () {
-		if (Input.GetMouseButtonDown(0)) {
+		if (Input.GetMouseButton(0)) {
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (collider.Raycast(ray, out hit, float.MaxValue)) {
+				GL.PushMatrix();
+				GL.LoadIdentity();
+				var projMat = Matrix4x4.Ortho(0f, _paintWallTex.width, 0f, _paintWallTex.height, -1f, 1f);
+				GL.LoadProjectionMatrix(projMat);
+				var uv = hit.textureCoord;
+				var brushPos = new Vector2(_paintWallTex.width * uv.x - 0.5f * brushTex.width, _paintWallTex.height * uv.y - 0.5f * brushTex.height);
+				RenderTexture.active = _paintWallTex;
+				Graphics.DrawTexture(new Rect(brushPos.x, brushPos.y, brushTex.width, brushTex.height), brushTex, brushMat);
+				RenderTexture.active = null;
+				GL.PopMatrix();
+			}
+		}
+
+		if (Input.GetMouseButtonDown(1)) {
 			RaycastHit hit;
 			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			if (collider.Raycast(ray, out hit, float.MaxValue)) {
@@ -111,11 +109,45 @@ public class RelativeTime : MonoBehaviour {
 
 		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_PREV, _clocksBuffer0);
 		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_CURR, _clocksBuffer1);
+		relativeTimeCompute.SetTexture(0, CS_PROP_HEAT_TEX, _paintWallTex);
 		relativeTimeCompute.SetFloat(CS_PROP_DELTA_TIME, Time.deltaTime);
 		relativeTimeCompute.Dispatch(0, numGroups, 1, 1);
 		var tmpBuffer = _clocksBuffer0; _clocksBuffer0 = _clocksBuffer1; _clocksBuffer1 = tmpBuffer;
 
 		Shader.SetGlobalBuffer(LIFE_PROPE_CLOCKS_CURR, _clocksBuffer0);
+
+		Graphics.Blit(null, _paintWallTex, attenMat);
+	}
+	
+	IEnumerator Cleaner() {
+		while (enabled) {
+			yield return new WaitForSeconds(1f);
+			_clocksBuffer0.GetData(_clocks);
+			for (var i = 0; i < _clocks.Length; i++) {
+				var c = _clocks[i];
+				if (c.t >= tEnd) {
+					c.t = -1f;
+					_clocks[i] = c;
+					Destroy(_flowers[i]);
+				}
+			}
+			_clocksBuffer0.SetData(_clocks);
+		}
+	}
+	IEnumerator Logger() {
+		var log = new StringBuilder();
+		while (enabled) {
+			yield return new WaitForSeconds(1f);
+			log.Length = 0;
+			_clocksBuffer0.GetData(_clocks);
+			for (var i = 0; i < 100; i++) {
+				var c = _clocks[i];
+				if (c.t < 0 || c.t >= tEnd)
+					continue;
+				log.AppendFormat("[{0:f}] ", c.t);
+			}
+			Debug.Log(log.ToString());
+		}
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
