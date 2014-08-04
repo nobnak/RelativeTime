9eb2ed5a1c5d87cc6e7c0d747bdbf19a57d6fb61
Nakata Nobuyuki <nobu.nakata@gmail.com>
2014-08-04 12:49:21 +0900

    ComputeBufferのデータをVertex Shaderで使う

---
+++ Assets/Life.shader
@@ -0,0 +1,46 @@
+﻿Shader "Custom/Life" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_TEnd ("End Time", Float) = 60.0
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		LOD 200
+		
+		CGPROGRAM
+		#pragma target 5.0
+		#pragma surface surf Lambert vertex:vert
+
+		sampler2D _MainTex;
+		float _TEnd;
+
+		#ifdef SHADER_API_D3D11
+		struct Clock {
+			float2 uv;
+			float t;
+		};
+		StructuredBuffer<Clock> clocksCurr;
+		int id;
+		#endif
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+		
+		void vert(inout appdata_full v, out Input o) {
+			UNITY_INITIALIZE_OUTPUT(Input, o);
+			#ifdef SHADER_API_D3D11
+			Clock c = clocksCurr[id];
+			v.vertex.xyz += v.normal * 2.0 * saturate(c.t / _TEnd);
+			#endif
+		}
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			half4 c = tex2D (_MainTex, IN.uv_MainTex);
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	} 
+	FallBack off
+}
--- Assets/RelativeTime.cs
+++ Assets/RelativeTime.cs
@@ -4,18 +4,23 @@ using System.Runtime.InteropServices;
 using System.Text;
 
 public class RelativeTime : MonoBehaviour {
-	public const string CS_PROP_CLOCKS_INPUT = "clocksIn";
-	public const string CS_PROP_CLOCKS_OUTPUT = "clocksOut";
+	public const string CS_PROP_CLOCKS_PREV = "clocksPrev";
+	public const string CS_PROP_CLOCKS_CURR = "clocksCurr";
 	public const string CS_PROP_DELTA_TIME = "dt";
 	public const int CS_NUM_THREADS = 256;
+
+	public const string LIFE_PROPE_CLOCKS_CURR = "clocksCurr";
+	public const string LIFE_PROP_ID = "id";
 
 	public int numClocks = CS_NUM_THREADS;
 	public float tEnd = 60f;
 	public ComputeShader relativeTimeCompute;
+	public GameObject flowerfab;
 
 	private int numGroups;
 	private Clock[] _clocks;
 	private ComputeBuffer _clocksBuffer0, _clocksBuffer1;
+	private GameObject[] _flowers;
 
 	void Start() {
 		numGroups = Mathf.CeilToInt((float)numClocks / CS_NUM_THREADS);
@@ -23,11 +28,13 @@ public class RelativeTime : MonoBehaviour {
 
 		_clocks = new Clock[numClocks];
 		for (var i = 0; i < _clocks.Length; i++)
-			_clocks[i] = new Clock(Vector2.zero, 0f);
+			_clocks[i] = new Clock(Vector2.zero, -1f);
 
 		_clocksBuffer0 = new ComputeBuffer(numClocks, Marshal.SizeOf(typeof(Clock)));
 		_clocksBuffer1 = new ComputeBuffer(numClocks, Marshal.SizeOf(typeof(Clock)));
 		_clocksBuffer0.SetData(_clocks);
+
+		_flowers = new GameObject[numClocks];
 
 		StartCoroutine(Logger());
 	}
@@ -57,11 +64,44 @@ public class RelativeTime : MonoBehaviour {
 	}
 
 	void Update () {
-		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_INPUT, _clocksBuffer0);
-		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_OUTPUT, _clocksBuffer1);
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (collider.Raycast(ray, out hit, float.MaxValue)) {
+				var id = -1;
+				_clocksBuffer0.GetData(_clocks);
+				for (var i = 0; i < _clocks.Length; i++) {
+					var c = _clocks[i];
+					if (c.t < 0) {
+						id = i;
+						c.t = 0f;
+						c.uv = hit.textureCoord;
+						_clocks[i] = c;
+						_clocksBuffer0.SetData(_clocks);
+						break;
+					}
+				}
+				if (id >= 0) {
+					if (_flowers[id] != null)
+						Destroy(_flowers[id]);
+
+					Debug.Log("Add flower");
+					var go = (_flowers[id] = (GameObject)Instantiate(flowerfab)).transform;
+					go.parent = transform;
+					go.position = hit.point;
+					var mat = go.renderer.material;
+					mat.SetInt(LIFE_PROP_ID, id);
+				}
+			}
+		}
+
+		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_PREV, _clocksBuffer0);
+		relativeTimeCompute.SetBuffer(0, CS_PROP_CLOCKS_CURR, _clocksBuffer1);
 		relativeTimeCompute.SetFloat(CS_PROP_DELTA_TIME, Time.deltaTime);
 		relativeTimeCompute.Dispatch(0, numGroups, 1, 1);
 		var tmpBuffer = _clocksBuffer0; _clocksBuffer0 = _clocksBuffer1; _clocksBuffer1 = tmpBuffer;
+
+		Shader.SetGlobalBuffer(LIFE_PROPE_CLOCKS_CURR, _clocksBuffer0);
 	}
 
 	[StructLayout(LayoutKind.Sequential)]
